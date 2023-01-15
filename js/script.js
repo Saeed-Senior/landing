08636a462340dc59ab9590841a61e617452b754d
Saeed-Senior <122204794+Saeed-Senior@users.noreply.github.com>
2023-01-15 19:30:40 +0300

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const isMobile = {
+   Android: function () {
+      return navigator.userAgent.match(/Android/i);
+   },
+   BlackBerry: function () {
+      return navigator.userAgent.match(/BlackBerry/i);
+   },
+   iOS: function () {
+      return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+   },
+   Opera: function () {
+      return navigator.userAgent.match(/Opera Mini/i);
+   },
+   Windows: function () {
+      return navigator.userAgent.match(/IEMobile/i);
+   },
+   any: function () {
+      return (
+         isMobile.Android() ||
+         isMobile.BlackBerry() ||
+         isMobile.iOS() ||
+         isMobile.Opera() ||
+         isMobile.Windows());
+   }
+};
+
+if (isMobile.any()) {
+   document.body.classList.add('_touch');
+} else {
+   document.body.classList.add('_pc');
+}
+
+const iconMenu = document.querySelector('.menu__icon');
+if (iconMenu) {
+const menuList = document.querySelector('.menu__list');
+iconMenu.addEventListener('click', function (e) {
+   iconMenu.classList.toggle('_active');
+   menuList.classList.toggle('_active');
+   document.body.classList.toggle('_lock');
+})
+}
